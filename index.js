4dce2694fcf0579ba35446e4acd3daea3f997c0e
Arman765 <friend.of.laden765@gmail.com>
2022-11-25 04:24:01 +0600

    update

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.upeallf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  const categoryCollection = client.db("cellsaleDB").collection("categories");
+
+  try {
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await categoryCollection.find(query).toArray();
+      res.send(categories);
+    });
+  } finally {
+  }
+};
+run().catch((error) => {
+  console.log(error);
+});
+
+app.get("/", async (req, res) => {
+  res.send("SERVER is running");
+});
+
+app.listen(port, () => {
+  console.log("Server running in port", port);
+});
